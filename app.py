597f56c1e789e1517d039e0a2fce194eb52810a6
ali-jee-786 <786syedalihussain@gmail.com>
2024-11-12 19:09:14 +0500

    updated

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def result():
         return render_template('index.html', error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
